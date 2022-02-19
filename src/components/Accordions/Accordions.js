d8ce86b9723c0d224245756843321f47afb08439
red room <99589226+redroom2340@users.noreply.github.com>
2022-02-19 07:27:43 -0800

    style css add __app.scss add

--- src/components/Accordions/Accordions.js
+++ src/components/Accordions/Accordions.js
@@ -9,27 +9,6 @@ const Accordions = () => {
         {/* <img src={articles} alt="logo" /> */}
         <h2>Accordions</h2>
       </div>
-      <div className="articles-data-min">
-        <div className="articles-data-col">
-          <h4 className="articles-name">Global HR Day 2022 is here</h4>
-          <p className="article-date">May 23, 2021</p>
-        </div>
-        <div className="articles-data-col">
-          <h4 className="articles-name">Sweden introduces new workplace culture in 2022</h4>
-          <p className="article-date">May 23, 2021</p>
-        </div>
-        <div className="articles-data-col">
-          <h4 className="articles-name">5 times it’s OK to fire an employee on FMLA leave</h4>
-          <p className="article-date">May 23, 2021</p>
-        </div>
-        <div className="articles-data-col">
-          <h4 className="articles-name">Japan updates certain employment laws</h4>
-          <p className="article-date">May 23, 2021</p>
-        </div>
-      </div>
-      <div className="articles-footer">
-        <h4>More</h4>
-      </div>
     </div>
   );
 };
